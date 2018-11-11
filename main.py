596dc40ab3f4d7c53a2bfb7fabc50b925071c574
London Lowmanstone <happylondon4@gmail.com>
2018-11-10 20:28:46 -0500

    Database working but without user data display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 if __name__ == "__main__":
     from tui import display, prompt, choice
+    from user_data import login, register, load_users, User
     from contextlib import contextmanager # TODO delete; just for testing
     
     class PlaceholderUser:
@@ -49,21 +50,16 @@ if __name__ == "__main__":
     # load plain term database (context manager)
     plain_term_database_loader = placeholder_plain_term_database_loader
 
-    # load user database (context manager)
-    user_database_loader = placeholder_user_database_loader
-    
-    login = placeholder_login
-    
     get_user_info = placeholder_get_user_info
     
-    with user_database_loader() as user_database:
+    with load_users() as user_database:
         with legal_term_database_loader() as legal_term_database:
             with plain_term_database_loader() as plain_term_database:
                 user = None
                 
                 while True:
-                    # ["Login (Logout)", "Search", "(See User Info)", Quit]
-                    options = []
+                    # ["Register", "Login (Logout)", "Search", "(See User Info)", Quit]
+                    options = ["Register"]
                     if user:
                         login_or_logout = "Logout"
                     else:
@@ -74,7 +70,11 @@ if __name__ == "__main__":
                         options.append("See User Info")
                     options.append("Quit")
                     user_choice = options[choice(options)]
-                    if user_choice == "Login":
+                    if user_choice == "Register":
+                        username = prompt("Username: ")
+                        password = prompt("Password: ")
+                        register(user_database, username, password)
+                    elif user_choice == "Login":
                         try:
                             username = prompt("Username: ")
                             password = prompt("Password: ")
