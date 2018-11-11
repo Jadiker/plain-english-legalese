a508aff85a386a1ea940b52a9ea093cda9cd0432
London Lowmanstone <happylondon4@gmail.com>
2018-11-10 21:08:08 -0500

    term database placeholders correct

--- main.py
+++ main.py
@@ -3,56 +3,27 @@ if __name__ == "__main__":
     from user_data import login, register, load_users, User, get_user_info
     from contextlib import contextmanager # TODO delete; just for testing
     
-    class PlaceholderUser:
-        def __init__(self, username, password, display_name):
-            self.username = username
-            self.password = password
-            self.display_name = display_name
-    
+    # TODO update the placeholders to the correct format
     def placeholder_search_function(dictionary, term):
         print("(Searching {} for term {})".format(dictionary, term))
         try:
-            return [dictionary[term]]
+            return dictionary[term]
         except KeyError:
-            return []
+            return ([], [])
         
     @contextmanager
-    def placeholder_legal_term_database_loader():
-        yield {"a legal term": "a legal value"}
+    def placeholder_term_database_loader():
+        yield {"some term": (["a legal value"], ["a plain value"])}
         
-    @contextmanager
-    def placeholder_plain_term_database_loader():
-        yield {"a plain term": "a plain value"}
         
-    @contextmanager
-    def placeholder_user_database_loader():
-        yield {"user": PlaceholderUser("user", "password", "Top Notch")}
-        
-    def placeholder_login(user_database, username, password):
-        print("Using placeholder login")
-        user = user_database[username]
-        if user.password != password:
-            raise RuntimeError("Bad password")
-        else:
-            return user
-            
-    def placeholder_get_user_info(user):
-        '''Returns a string with information about the user'''
-        return "User '{}' with username '{}' and password {}".format(user.display_name, user.username, "*"*len(user.password)) 
-
-
     # takes a dictionary and a term to search for in the dictionary
     search_function = placeholder_search_function
 
     # load term database (context manager)
-    legal_term_database_loader = placeholder_legal_term_database_loader
+    term_database_loader = placeholder_term_database_loader
     
-    # load plain term database (context manager)
-    plain_term_database_loader = placeholder_plain_term_database_loader
-
     with load_users() as user_database:
-        with legal_term_database_loader() as legal_term_database:
-            with plain_term_database_loader() as plain_term_database:
+        with term_database_loader() as term_database:
                 user = None
                 
                 while True:
@@ -72,6 +43,9 @@ if __name__ == "__main__":
                         username = prompt("Username: ")
                         password = prompt("Password: ")
                         register(user_database, username, password)
+                        print("User successfully registered!")
+                        login(user_database, username, password)
+                        print("User successfully logged in!")
                     elif user_choice == "Login":
                         try:
                             username = prompt("Username: ")
@@ -85,11 +59,10 @@ if __name__ == "__main__":
                         display("Successfully logged out!")
                     elif user_choice == "Search":
                         term = prompt("What term would you like to search for? ").strip()
-                        legal_response = search_function(legal_term_database, term)
-                        plain_response = search_function(plain_term_database, term)
+                        legal_responses, plain_responses = search_function(term_database, term)
                         
-                        display("Here are some legal definitions of {}:\n{}".format("\n".join(term, legal_response)))
-                        display("Here are the plain English definitions of {}:\n{}".format("\n".join(term, plain_response)))
+                        display("Here are some legal definitions of '{}':\n\n{}\n".format(term, "\n".join(legal_responses)))
+                        display("Here are the plain English definitions of '{}':\n\n{}\n\n".format(term, "\n".join(plain_responses)))
                     elif user_choice == "See User Info":
                         display("\n" + get_user_info(user) + "\n")
                     elif user_choice == "Quit":
@@ -97,5 +70,3 @@ if __name__ == "__main__":
                         break
                     else:
                         print("Something went wrong in the program. Option {} was not recognized".format(user_choice))
-                            
-                
